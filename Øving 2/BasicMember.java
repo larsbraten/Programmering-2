d5f85544d812d1a6dcc34f0ce57bceb37be9276e
Lars-Håvard Bråten <larshavard.holter.braten@gmail.com>
2020-02-02 22:15:41 +0100

    Øving 2, 2020

---
+++ Øving 2/BasicMember.java
@@ -0,0 +1,12 @@
+import java.time.LocalDate;
+
+public class BasicMember extends BonusMember  {
+    public BasicMember(int memberNo, Personals personals, LocalDate enrolledDate) {
+        super(memberNo, personals, enrolledDate);
+    }
+
+    public BasicMember(int memberNo, Personals personals, LocalDate enrolledDate, int point) {
+        super(memberNo, personals, enrolledDate, point);
+
+    }
+}
---
+++ Øving 2/GoldMember.java
@@ -0,0 +1,16 @@
+import java.time.LocalDate;
+
+public class GoldMember  extends BonusMember {
+
+    public GoldMember(int memberNo, Personals personals, LocalDate enrolledDate, int point) {
+        super(memberNo, personals, enrolledDate, point);
+    }
+
+    /**
+     * Metode som registrerer poeng
+     * @param points poeng
+     */
+    public void registerPoints(int points) {
+        super.registerPoints((int) (points * factorGold));
+    }
+}
